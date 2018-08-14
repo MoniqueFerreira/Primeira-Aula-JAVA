e8debb7ab96ecbfc5b33cd74b4eb613f92dd827a
MoniqueFerreira <moniquesz23@gmail.com>
2018-08-13 22:40:18 -0300

    Primeira Aula JAVA

---
+++ src/aula/Class.java
@@ -0,0 +1,14 @@
+package aula;
+
+public class Class {
+	public static void main(String args[]) {
+		
+	//Variável do tipo primitivo inteiro
+	int numero = 1, numero2 = 2, numero3 = 3, numero4 = 4, numero5 = 5, numero6 = 6, numero7 = 7, numero8 = 8, numero9 = 9, numero0 = 0;
+	drawLine  
+	
+	//As regras das operações matemáticas se aplicam normalmente
+	
+	System.out.print(numero9 * numero9);
+	}
+}	
