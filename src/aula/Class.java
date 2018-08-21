0c822d95d6e97e7001b5f814586fccca540ac378
MoniqueFerreira <moniquesz23@gmail.com>
2018-08-20 22:44:58 -0300

    Segunda-Aula-JAVA

--- src/aula/Class.java
+++ src/aula/Class.java
@@ -4,11 +4,50 @@ public class Class {
 	public static void main(String args[]) {
 		
 	//Variável do tipo primitivo inteiro
-	int numero = 1, numero2 = 2, numero3 = 3, numero4 = 4, numero5 = 5, numero6 = 6, numero7 = 7, numero8 = 8, numero9 = 9, numero0 = 0;
-	drawLine  
+	int numero = 0, numero2 = 2;
 	
 	//As regras das operações matemáticas se aplicam normalmente
 	
-	System.out.print(numero9 * numero9);
+	System.out.println(numero * numero2); 
+	
+	
+//System.out.println = mostrar o resultado.
+
+/* Operadores Lógicos
+* > -> Maior que
+* < -> Menor que
+* == -> igual
+* >= -> Maior ou igual
+* <= -> Menor ou igual
+* != -> Diferente
+* && -> E
+* || -> Ou
+* */
+
+/* while e for são tipos de laços de repetição.
+*Exemplo de while: 
+*while(i <= 10000) {
+*			System.out.println("TP é maravilhoso");
+*			i = i + 1;
+*= enquanto i for menor ou igual a 1000, se repetirá "TP é maravilhoso" como o resultado, i é a condição".			
+*
+*Exemplo de for:
+*for(int x = 10; x > 0; x = x - 1) {
+*			System.out.println(x);
+*= x = 10, porém enquanto x for menor que 0, será retirado 1 de x;
+*/
+
+for(int x = 350; x > 0; x = x - 75) {
+			System.out.println(x);
+
+
+
+
+while(numero < 10) {
+			System.out.println("Hey");
+			numero = numero +1;
+			}	
+		}	
 	}
-}	
+}
+
