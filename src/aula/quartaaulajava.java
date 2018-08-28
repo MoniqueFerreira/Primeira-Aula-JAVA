43be1852aacb7cba9156e0d76c69f4ff9e4023d9
Aluno <moniquesz23@gmail.com>
2018-08-27 22:47:35 -0300

    aula4

--- src/aula/quartaaulajava.java
+++ src/aula/quartaaulajava.java
@@ -6,14 +6,29 @@ public class quartaaulajava {
 		int total = n1 + n2;
 		return total;
 	}
+	public static double subtracao(int n1, int n2) {
+		double total = n1 - n2;
+		return total;
+	}
 	public static double mult(int n1, int n2) {
 	double total = n1 * n2;
 	return total;
 	}
-	
+	public static double divisao(int n1, int n2) {
+		double total = n1 / n2;
+		return total;
+	}
+	public static double expo(int n1, int n2) {
+		double total = n1 ^ n2;
+		return total;
+	}
 	public static void main(String[] args) {
 		System.out.println(adicao(10,2));
+		System.out.println(subtracao(10,2));
 		System.out.println(mult(10,2));
+		System.out.println(divisao(10,2));
+		System.out.println(expo(10,2));
+		
 	}
 }
 
